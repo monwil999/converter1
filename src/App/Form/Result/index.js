a6ef2f6f4725c441e9e29fee5c524c88a0aefb89
monwil999 <monikawilkpr@gmail.com>
2024-12-09 19:40:46 +0100

    initial commit

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 
+export const Result = ({ result }) => {
+  if (!result || result.sourceAmount === undefined || result.targetAmount === undefined) {
+    return <p>Brak danych do wyświetlenia wyników.</p>;
+  }
 
-export const Result = ({ result }) => (
+  return (
     <p className="result">
-        {result !== undefined && (
-            <>
-            {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
-            {""}
-            <strong>
-                {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-            </strong>
-            </>
-        )}
+      {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=&nbsp;
+      <strong>
+        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+      </strong>
     </p>
-
-);
+  );
+};
